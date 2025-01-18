9366da0d0a55189a658e37f862adfa0eb6eddb3b
Pi-0r-Tau <147527008+Pi-0r-Tau@users.noreply.github.com>
2025-01-18 13:02:57 +0000

    AppDelegate Version 0.0.1 Cust need to added

---
+++ EpilepsyFlashProtectionApp/AppDelegate.swift
@@ -0,0 +1,21 @@
+import SwiftUI
+
+@main
+struct EpilepsyFlashProtectionApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
